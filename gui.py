6b76f110f7c16118d34f0f4f19df813949f96033
Yifan <yifanyang61@gmail.com>
2023-01-15 23:03:21 -0600

    Correct some mistakes

--- gui.py
+++ gui.py
@@ -14,11 +14,13 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button(size=2, image_source="add.png", mouseover_colors="LightBlue",
                        tooltip="Add Todo", key="Add")
+#add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button(size=2, image_source="complete.png",  mouseover_colors="LightBlue",
                             tooltip="Oh yeah! Complete!", key="Complete")
+#complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
@@ -62,7 +64,10 @@ while True:
         case "Exit":
             break
         case "todos":
-            window["todo"].update(value=values["todos"][0])
+            try:
+                window["todo"].update(value=values["todos"][0])
+            except IndexError:
+                sg.popup("Please add something first.", font=('Helvetica', 20))
         case sg.WIN_CLOSED:
             break
 
